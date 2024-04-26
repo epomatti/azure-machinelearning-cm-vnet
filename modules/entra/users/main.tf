b4cc56475bdd1df308957efb9883fb66367130b8
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-04-26 13:47:18 -0300

    fix: administrator will use the correct name

--- modules/entra/users/main.tf
+++ modules/entra/users/main.tf
@@ -7,7 +7,7 @@ resource "azuread_user" "data_scientist" {
 
 resource "azuread_user" "administrator" {
   account_enabled     = true
-  user_principal_name = "${var.data_scientist_username}@${var.tenant_domain}"
+  user_principal_name = "${var.administrator_username}@${var.tenant_domain}"
   display_name        = var.administrator_username
   password            = var.user_password
 }
