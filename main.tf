454aad2486737248e5c38a3ca08107e940ee9210
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-05-22 12:08:33 -0300

    feat: upgraded terraform providers

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.101.0"
+      version = "3.104.2"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.48.0"
+      version = "2.50.0"
     }
   }
 }
